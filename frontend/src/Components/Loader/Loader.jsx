320b02dfaf86792fbb0900cb0b223aeaa569abb3
mukesh prajapat <mukeshprajapat7529@gmail.com>
2022-03-04 11:04:42 +0530

    Initial commit

---
+++ frontend/src/Components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Loader.css';
+const Loader = () => {
+    return (
+        <div class="sk-chase" style={{margin:"260px 50px 200px 650px"}}>
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+</div>
+    )
+}
+
+export default Loader 
---
+++ frontend/src/Components/NewPost/NewPost.jsx
@@ -0,0 +1,73 @@
+import { Button, Typography } from "@mui/material";
+import React, { Fragment,useEffect, useState } from "react";
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewPost } from "../../Actions/Post";
+import { loadUser } from "../../Actions/User";
+import "./NewPost.css";
+import MetaData from "../MetaData/MetaData";
+const NewPost = () => {
+  const [image, setImage] = useState(null);
+  const [caption, setCaption] = useState("");
+
+  const { loading, error, message } = useSelector((state) => state.like);
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    const Reader = new FileReader();
+    Reader.readAsDataURL(file);
+
+    Reader.onload = () => {
+      if (Reader.readyState === 2) {
+        setImage(Reader.result);
+      }
+    };
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    await dispatch(createNewPost(caption, image));
+    dispatch(loadUser());
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch({ type: "clearErrors" });
+    }
+
+    if (message) {
+      alert.success(message);
+      dispatch({ type: "clearMessage" });
+    }
+  }, [dispatch, error, message, alert]);
+
+  return (
+    <Fragment>
+      <MetaData title="New Post"/>
+    
+    <div className="newPost">
+      <form className="newPostForm" onSubmit={submitHandler}>
+        <Typography variant="h4">New Post</Typography>
+
+        {image && <img src={image} alt="post" />}
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <input
+          type="text"
+          placeholder="Caption..."
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+        />
+        <Button disabled={loading} type="submit">
+          Post
+        </Button>
+      </form>
+    </div>
+    </Fragment>
+  );
+};
+
+export default NewPost;
